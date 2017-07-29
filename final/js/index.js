6fcd54b0c9a509ab1c7c721c9678250ebd72e256
coachlin <s6h0a5n1e2@gmail.com>
2017-07-29 15:42:57 +0800

    final

--- final/js/index.js
+++ final/js/index.js
@@ -5,6 +5,7 @@ var rand = function(start, end) {
     return f + start;
 }
 
+
 $('#data').empty();
 card = [];
 var poker = [];
@@ -23,10 +24,96 @@ for (var i = 1; i <= 520; i++) {
 for (var i = 0; i < 10; i++) {
     card.push(poker[i]);
 }
-var i = 0;
+var i = 3;
 var count = 0;
 var count1 = 0;
+var ace = 0;
+var ace1 = 0;
 
+$('#start').on('click', function() {
+    document.getElementById('data1').innerHTML = "";
+    $img = $('<img>').attr('src', './poker/pic' + card[0] + '.png');
+    $col = $('<div>').attr('class', 'col-1 text-center').append($img);
+    $('#data').append($col);
+
+    $img = $('<img>').attr('src', './poker/back.png');
+    $col = $('<div>').attr('class', 'col-1 text-center').append($img);
+    $('#data1').append($col);
+
+    $img = $('<img>').attr('src', './poker/pic' + card[2] + '.png');
+    $col = $('<div>').attr('class', 'col-1 text-center').append($img);
+    $('#data').append($col);
+
+    $img = $('<img>').attr('src', './poker/pic' + card[3] + '.png ');
+    $col = $('<div>').attr('class', 'col-1 text-center').append($img);
+    $('#data1').append($col);
+
+    var c1 = card[0] - 1;
+    point1 = Math.floor(c1 / 4) + 1;
+    if (point1 > 10) {
+        point1 = 10;
+    }
+    if (point1 == 1) {
+        if (count + 11 <= 21) {
+            point1 = 11;
+            ace++;
+        } else {
+            point1 = 1;
+        }
+    }
+    count = count + point1;
+    var c1 = card[2] - 1;
+    point1 = Math.floor(c1 / 4) + 1;
+    if (point1 > 10) {
+        point1 = 10;
+    }
+    if (point1 == 1) {
+        if (count + 11 <= 21) {
+            point1 = 11;
+            ace++;
+        } else {
+            point1 = 1;
+        }
+    }
+    count = count + point1;
+    var c2 = card[1] - 1;
+    point2 = Math.floor(c2 / 4) + 1;
+    if (point2 > 10) {
+        point2 = 10;
+    }
+    if (point2 == 1) {
+        if (count + 11 <= 21) {
+            point2 = 11;
+            ace1++;
+        } else {
+            point2 = 1;
+        }
+    }
+    count1 = count1 + point2;
+    var c2 = card[3] - 1;
+    point2 = Math.floor(c2 / 4) + 1;
+    if (point2 > 10) {
+        point2 = 10;
+    }
+    if (point2 == 1) {
+        if (count + 11 <= 21) {
+            point2 = 11;
+            ace1++;
+        } else {
+            point2 = 1;
+        }
+    }
+    count1 = count1 + point2;
+    $('#count').val(count);
+
+    i++;
+    if (count == 21) {
+        $('#result').val("BLACK JACK!!!");
+    }
+    if (count > 21) {
+        $('#result').val("YOU LOOSE!!!");
+    }
+});
 
 $('#ask').on('click', function() {
 
@@ -40,36 +127,95 @@ $('#ask').on('click', function() {
     if (point > 10) {
         point = 10;
     }
+    //判斷A為1或11
+    if (point == 1) {
+        if (count + 11 <= 21) {
+            point = 11;
+            ace++;
+        } else {
+            point = 1;
+        }
+    }
     count = count + point;
     $('#count').val(count);
     i++;
     if (count == 21) {
-        $('#count').val("BLACK JACK!!!");
+        $('#result').val("BLACK JACK!!!");
+        win++;
     }
     if (count > 21) {
-        $('#count').val("YOU LOOSE!!!");
+        if (ace == 0) {
+            $('#result').val("YOU LOOSE!!!");
+            lose++;
+        } else if (ace == 1) {
+            count -= 10;
+            $('#count').val(count);
+            ace = ace - 1;
+        }
     }
+    if (i == 7 && count < 21) {
+        $('#result').val("YOU WIN!!!");
+    }
+
 });
 
 $('#stop').on('click', function() {
-
-    $img = $('<img>').attr('src', './poker/pic' + card[i] + '.png');
+    document.getElementById('data1').innerHTML = "";
+    $img = $('<img>').attr('src', './poker/pic' + card[1] + '.png');
     $col = $('<div>').attr('class', 'col-1 text-center').append($img);
     $('#data1').append($col);
+    $img = $('<img>').attr('src', './poker/pic' + card[3] + '.png');
+    $col = $('<div>').attr('class', 'col-1 text-center').append($img);
+    $('#data1').append($col);
+    while (count1 < 17) {
+        $img = $('<img>').attr('src', './poker/pic' + card[i] + '.png');
+        $col = $('<div>').attr('class', 'col-1 text-center').append($img);
+        $('#data1').append($col);
 
-
-    var c = card[i] - 1;
-    point = Math.floor(c / 4) + 1;
-    if (point > 10) {
-        point = 10;
+        var c = card[i] - 1;
+        point1 = Math.floor(c / 4) + 1;
+        if (point1 > 10) {
+            point1 = 10;
+        }
+        //判斷A為1或11
+        if (point1 == 1) {
+            if (count1 + 11 <= 21) {
+                point1 = 11;
+                ace1++;
+            } else {
+                point1 = 1;
+            }
+        }
+        count1 = count1 + point1;
+        $('#count1').val(count1);
+        i++;
     }
-    count1 = count1 + point;
-    $('#count1').val(count1);
-    i++;
-    if (count1 == 21) {
-        $('#count1').val("BLACK JACK!!!");
+    if (count1 > count && count1 <= 21) {
+        $('#count').val(count + "<" + count1);
+        $('#result').val("YOU LOSE!!!");
+    }
+    if (count1 < count && count <= 21) {
+        $('#count').val(count + ">" + count1);
+        $('#result').val("YOU WIN!!!");
     }
     if (count1 > 21) {
-        $('#count1').val("YOU LOOSE!!!");
+        if (ace1 == 0) {
+            $('#result').val("YOU WIN!!!");
+        } else if (ace1 == 1) {
+            count1 -= 10;
+            ace1 = ace1 - 1;
+        }
     }
+    if (count1 == count) {
+        $('#count').val(count + "=" + count1);
+        $('#result').val("Tie Game");
+    }
+
 });
+
+
+
+$('#reset').on('click', function() {
+    window.location.reload();
+});
+$('#count').val(count);
